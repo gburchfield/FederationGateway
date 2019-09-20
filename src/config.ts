1369166ec3aa894fbd9a1a781ede6355d496409b
Glen Burchfield <glen.burchfield@baker-taylor.com>
2019-09-20 16:32:00 -0400

    Clean config

--- src/config.ts
+++ src/config.ts
@@ -5,32 +5,12 @@ let config: ConfigParent = {
     dev: {
         port: parseInt(process.env.PORT),
         secret:process.env.SECRET_KEY,
-        services_string: process.env.SERVICES_STRING,
-        services: [
-            {
-                name: "Authentication",
-                url: "http://localhost:4001/graphql"
-            },
-            {
-                name: "Profile",
-                url: "http://localhost:4002/graphql"
-            }
-        ]
+        services_string: process.env.SERVICES_STRING
     },
     prod: {
         port: parseInt(process.env.PORT),
         secret: process.env.SECRET_KEY,
-        services_string: process.env.SERVICES_STRING,
-        services: [
-            {
-                name: "Authentication",
-                url: "http://localhost:4001/graphql"
-            },
-            {
-                name: "Profile",
-                url: "http://localhost:4002/graphql"
-            }
-        ]
+        services_string: process.env.SERVICES_STRING
     }
 }
 
@@ -46,7 +26,6 @@ interface Config {
     port: number,
     secret: string,
     services_string: string,
-    services: Array<Service>
 }
 
 export interface Service {
