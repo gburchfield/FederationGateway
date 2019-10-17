941986b9a225a3e51c3d958def746bc226b6c6e1
Glen Burchfield <glen.burchfield@baker-taylor.com>
2019-10-17 08:26:18 -0400

    changes needed for n.b.p. implementation of services

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import dotenv from 'dotenv'
 import {GatewayNodeConfig} from "socialbrokernode/dist/lib/Types";
-import {SocialBrokerNode} from "socialbrokernode/dist/lib/SocialBrokerNode";
+import {GatewayNode} from "socialbrokernode";
+import {ServiceEndpointDefinition} from "@apollo/gateway";
 dotenv.config()
+
+const getServices: () => ServiceEndpointDefinition[] = () => {
+    let services: ServiceEndpointDefinition[] = []
+    let serviceStr = process.env.SERVICES_STRING
+    let serviceArr = serviceStr.split(";")
+
+    serviceArr.forEach((item) => {
+        let item_arr = item.split(":")
+        let service = {
+            name: item_arr[0],
+            url: `http://${item_arr[1]}:${item_arr[2]}/graphql`
+        }
+        services.push(service)
+    })
+
+    return services
+}
 
 
 const gatewayConfig: GatewayNodeConfig = {
     port: process.env.PORT,
     secret: process.env.SECRET_KEY,
-    services: [
-        {
-            name: "Authentication",
-            url: "http://authentication_service:4001/graphql"
-        },
-        {
-            name: "Profile",
-            url: "http://profile_service:4002/graphql"
-        }
-    ]
+    services: getServices()
 };
 
-const MySocialBrokerGateway: SocialBrokerNode = new SocialBrokerNode(gatewayConfig);
-
+const MySocialBrokerGateway: GatewayNode = new GatewayNode(gatewayConfig);
+MySocialBrokerGateway.buildGatewayServer()
 MySocialBrokerGateway.start();
